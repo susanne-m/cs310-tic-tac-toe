c4405e9eba1384a48606f4992da632e17970b810
Susanne McGinnis <smcginnis@stu.jsu.edu>
2020-02-25 17:13:52 -0600

    Fixed TicTacToe View

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class TicTacToeView {
     
     private final Scanner keyboard;
+    int row = 0;
+    int col =0;
+    public static TicTacToeMove moves;
     
     /* CONSTRUCTOR */
 	
@@ -13,18 +16,34 @@ public class TicTacToeView {
         /* Initialize scanner (for console keyboard) */
         
         keyboard = new Scanner(System.in);
-        
-    }
-	
+        moves = new TicTacToeMove( row,col);   
+    }  
     public TicTacToeMove getNextMove(boolean isXTurn) {
         
         /* Prompt the player to enter the row and the column of their next move.
            Return as a TicTacToeMove object. */
         
         // INSERT YOUR CODE HERE
-
-        return null; // remove this line later!
-
+        
+        if(isXTurn == true){
+            System.out.println("Player 1 (X) Move:");
+            
+        }
+        else{         
+            System.out.println("Player 2 (O) Move:");
+        }
+                System.out.println("Enter the row and column numbers, separated by a space:");
+             String move = keyboard.nextLine();
+            String[] parts = move.split(" ");
+            int[] moveInt = new int[parts.length];
+            for(int i = 0; i < parts.length; i++) {
+                moveInt[i] = Integer.parseInt(parts[i]);
+            }
+            int r = moveInt[0];
+            int c = moveInt[1];
+            moves.row = r;
+            moves.col = c;
+            return moves;   
     }
 
     public void showInputError() {
@@ -43,6 +62,5 @@ public class TicTacToeView {
         
         System.out.println("\n\n" + board);
         
-    }
-	
+    }	
 }
